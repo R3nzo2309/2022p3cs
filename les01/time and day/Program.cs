e82cf057e2e2fbbfeef50783001ec3904e8e5b29
Renzo <reemer2002@gmail.com>
2022-02-14 13:09:11 +0100

    time and day

---
+++ les01/time and day/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace time_and_day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateNow = DateTime.Now;
+            Console.WriteLine("de tijd is: {0}", dateNow);
+        }
+    }
+}
